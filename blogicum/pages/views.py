c3bd759fe9284ee303461f240ea7ef82e780d52b
Nikolay Oleynik <nikolayoleynik7@mail.ru>
2025-05-17 19:54:45 +0300

    PEP8

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 
 def about(request):
-    """Статическая страница "О нас" """
     return render(request, 'about.html')
 
 
 def rules(request):
-    """Статическая страница "Правила" """
     return render(request, 'rules.html')
